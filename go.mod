724b59e648ba7d95c8d35e135290cf6428047a37
ernestkorir <ernestbore3@gmail.com>
2024-12-28 12:26:28 +0300

    First Go code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module compile/main
+
+go 1.23.0
--- main.go
+++ main.go
@@ -1,1 +1,12 @@
-package let_sGo
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	x := 42
+	y := "James Bond"
+	z := true
+	fmt.Println(x, y, z)
+}
