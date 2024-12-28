87b8eb0dcd9adbe23746c4be89d9290d65dc8bbf
ernestkorir <ernestbore3@gmail.com>
2024-12-28 20:17:09 +0300

    typeof more

--- main.go
+++ main.go
@@ -5,10 +5,15 @@ import "fmt"
 type erno int
 
 var x erno
+var y int
 
 func main() {
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 	x = 42
 	fmt.Println(x)
+	y = int(x)
+	fmt.Println(y)
+	fmt.Printf("%T\n", y)
+
 }
