fdf023b77d9e71d6df2fc2f97113a4ded56dd38c
ernestkorir <ernestbore3@gmail.com>
2024-12-29 12:28:07 +0300

    Print hex dec and binary

--- main.go
+++ main.go
@@ -2,18 +2,8 @@ package main
 
 import "fmt"
 
-const (
-	a = iota
-	b
-	c
-)
-
 func main() {
-	fmt.Println(a)
-	fmt.Println(b)
-	fmt.Println(c)
-	fmt.Printf("%T\n", a)
-	fmt.Printf("%T\n", b)
-	fmt.Printf("%T\n", c)
+	a := 42
+	fmt.Printf("%d\t%b\t%#x", a, a, a)
 
 }
